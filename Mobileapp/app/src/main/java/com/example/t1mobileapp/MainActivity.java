d901e39474b38b7fff35e2871d3b747e5dcc1d72
Dennis <alejimenezc@hotmail.com>
2022-03-07 17:14:10 -0600

    Lector de Codigos base

--- Mobileapp/app/src/main/java/com/example/t1mobileapp/MainActivity.java
+++ Mobileapp/app/src/main/java/com/example/t1mobileapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.t1mobileapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                IntentIntegrator integrador = new IntentIntegrator(MainActivity.this);
+                integrador.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrador.setPrompt("Escaneo de Codigo");
+                integrador.setCameraId(0);
+                integrador.setBarcodeImageEnabled(true);
+                integrador.initiateScan();
+            }
+        });
+
     }
 }
