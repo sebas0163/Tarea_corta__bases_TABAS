d8e2bed42afb02f4f7c03ff87bbccc39cf5c3e0a
Steve <stevealv@hotmail.com>
2022-03-09 04:51:30 -0600

    [Update] Login

--- Aplicación web/TABAS/src/app/login/login.component.ts
+++ Aplicación web/TABAS/src/app/login/login.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ApiService } from '../services/api.service';
 import { LoginI } from '../models/login.interface';
+import { User } from '../classes/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -16,16 +18,35 @@ export class LoginComponent implements OnInit {
     access: new FormControl('Admin', Validators.required)
   })
 
-  constructor(private api: ApiService) { }
+  masterUser = new User(
+    "master@tabas.com",
+    "master123"
+  );
+
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onLogin(form: LoginI) {
-    this.api.loginByEmail(form).subscribe(data => {
-      console.log(data)
-    })
+    // this.api.loginByEmail(form).subscribe(data => {
+    //   console.log(data)
+    // })
 
+
+  }
+
+  login() {
+    let femail = this.loginForm.get('email')?.value;
+    let fpassword = this.loginForm.get('password')?.value;
+    console.log(femail);
+    console.log(fpassword);
+    if (femail == this.masterUser.email &&
+      this.masterUser.password == fpassword) {
+      this.router.navigate(['baggage']);
+    } else {
+      console.log("Datos incorrectos")
+    }
   }
 
 }
