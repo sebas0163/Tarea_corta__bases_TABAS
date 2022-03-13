55e2d658731f033d59b1b3288122a5f0c9eed7f2
Steve <stevealv@hotmail.com>
2022-03-13 00:09:37 -0600

    [Add] XML e-bill

--- Aplicación web/TABAS/src/app/baggage/baggage.component.ts
+++ Aplicación web/TABAS/src/app/baggage/baggage.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Suitcase } from '../classes/suitcase';
 import { DataService } from '../services/data.service';
+import { Bill } from '../classes/bill';
 
 @Component({
   selector: 'app-baggage',
@@ -21,6 +22,7 @@ export class BaggageComponent implements OnInit {
   headers = ["Número de maleta", "Usuario", "Color", "Peso", "Costo de envío", "Vuelo"]
 
   ngOnInit(): void {
+    generateBill();
   }
 
   addBaggage() {
@@ -28,3 +30,64 @@ export class BaggageComponent implements OnInit {
   }
 
 }
+function generateBill() {
+
+  let e = "e";
+  let bill = new Bill(e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e,
+    e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e, e);
+
+  var doc = document.implementation.createDocument("", "", null);
+  var peopleElem = doc.createElement("people");
+
+  var personElem1 = doc.createElement("person");
+  personElem1.setAttribute("first-name", "eric");
+  personElem1.setAttribute("middle-initial", "h");
+  personElem1.setAttribute("last-name", "jung");
+
+  var addressElem1 = doc.createElement("address");
+  addressElem1.setAttribute("street", "321 south st");
+  addressElem1.setAttribute("city", "denver");
+  addressElem1.setAttribute("state", "co");
+  addressElem1.setAttribute("country", "usa");
+  personElem1.appendChild(addressElem1);
+
+  var addressElem2 = doc.createElement("address");
+  addressElem2.setAttribute("street", "123 main st");
+  addressElem2.setAttribute("city", "arlington");
+  addressElem2.setAttribute("state", "ma");
+  addressElem2.setAttribute("country", "usa");
+  personElem1.appendChild(addressElem2);
+
+  var personElem2 = doc.createElement("person");
+  personElem2.setAttribute("first-name", "jed");
+  personElem2.setAttribute("last-name", "brown");
+
+  var addressElem3 = doc.createElement("address");
+  addressElem3.setAttribute("street", "321 north st");
+  addressElem3.setAttribute("city", "atlanta");
+  addressElem3.setAttribute("state", "ga");
+  addressElem3.setAttribute("country", "usa");
+  personElem2.appendChild(addressElem3);
+
+  var addressElem4 = doc.createElement("address");
+  addressElem4.setAttribute("street", "123 west st");
+  addressElem4.setAttribute("city", "seattle");
+  addressElem4.setAttribute("state", "wa");
+  addressElem4.setAttribute("country", "usa");
+  personElem2.appendChild(addressElem4);
+
+  var addressElem5 = doc.createElement("address");
+  addressElem5.setAttribute("street", "321 south avenue");
+  addressElem5.setAttribute("city", "denver");
+  addressElem5.setAttribute("state", "co");
+  addressElem5.setAttribute("country", "usa");
+  personElem2.appendChild(addressElem5);
+
+  peopleElem.appendChild(personElem1);
+  peopleElem.appendChild(personElem2);
+  doc.appendChild(peopleElem);
+
+  console.log(bill.create());
+
+}
+
