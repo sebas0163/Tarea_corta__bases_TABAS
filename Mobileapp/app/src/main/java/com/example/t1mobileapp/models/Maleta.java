7fd416ecc3feded1d13045f3fbcafaacb2f8bb5e
Dennis <alejimenezc@hotmail.com>
2022-03-13 23:43:21 -0600

    Conexion corriendo pero error al intentar vincular con API

---
+++ Mobileapp/app/src/main/java/com/example/t1mobileapp/models/Maleta.java
@@ -0,0 +1,49 @@
+package com.example.t1mobileapp.models;
+
+public class Maleta {
+    private String peso;
+    private String costo;
+    private String numero;
+    private String color;
+    private String estado;
+
+    public String getPeso() {
+        return peso;
+    }
+
+    public void setPeso(String peso) {
+        this.peso = peso;
+    }
+
+    public String getCosto() {
+        return costo;
+    }
+
+    public void setCosto(String costo) {
+        this.costo = costo;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
