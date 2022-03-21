150247a6575b46fdfd009b6608ed12126b333b2b
Sebas <sebas0163@hotmail.com>
2022-03-21 16:51:10 -0600

    Arreglos CSS

--- Aplicación web/TABAS/src/app/app.component.ts
+++ Aplicación web/TABAS/src/app/app.component.ts
@@ -3,9 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-            <div class="open-card-BG" *ngIf="registerView == 'regView1'"><nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="open-card-BG" *ngIf="registerView == 'regView1'"><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-              <a class="navbar-brand" href="#">TABAS</a>
+              <a class="navbar-brand" href="#">
+              <img src="assets/images/minilogo.png" width="50" height="45">TABAS</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -17,11 +18,13 @@ import { Component } from '@angular/core';
               </div>
             </div>
           </nav>
-          
+
           <router-outlet></router-outlet></div>
-            <div class="open-card-BG" *ngIf="registerView == 'regView2'"><nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="open-card-BG" *ngIf="registerView == 'regView2'"><nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <div class="container-fluid">
-              <a class="navbar-brand" href="/baggage">TABAS</a>
+              <a class="navbar-brand" href="/baggage">
+              <img src="assets/images/minilogo.png" width="50" height="45">
+              TABAS</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
                 aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -39,7 +42,7 @@ import { Component } from '@angular/core';
               </div>
             </div>
           </nav>
-          
+
           <router-outlet></router-outlet></div>
             `,
   // templateUrl: './app.component.html',
